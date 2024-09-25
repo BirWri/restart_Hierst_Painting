3d39c31e55b1651cc4a8e0b90ebb5129aebe8c7e
BirWri <55280458+BirWri@users.noreply.github.com>
2024-09-25 15:02:15 +0200

    Adding comments and refractor

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import colorgram
 
-colors = colorgram.extract('pikachu.jpg', 6)
-print(colors)
-list_of_colors = []
 
-#TODO transform the info into a list of just the colors
-for x in range(0,len(colors)-1):
-    r = colors[x].rgb.r
-    g = colors[x].rgb.g
-    b = colors[x].rgb.b
-    new_color = (r, g, b)
-    list_of_colors.append(new_color)
+def extract_colors_from_image():
+    """ Extract colors from an image"""
+    colors = colorgram.extract('pikachu.jpg', 10)
+    return colors
 
-print(list_of_colors)
 
+def colors_to_rgb_format(colors):
+    """Transform the extracted data to a list of tuples for rgb"""
+    list_of_colors = []
+    for color in colors:
+        r = color.rgb.r
+        g = color.rgb.g
+        b = color.rgb.b
+        new_color = (r, g, b)
+        list_of_colors.append(new_color)
+    return list_of_colors
+
+
+colors = extract_colors_from_image()
+rgb = colors_to_rgb_format(colors)
+print(rgb)
 
