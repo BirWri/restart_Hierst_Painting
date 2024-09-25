9b30c5e99d8a9092dcb91098b79dab703aea4ba5
BirWri <55280458+BirWri@users.noreply.github.com>
2024-09-25 16:10:54 +0200

    Draw a 10x10 dot canvas

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import random
+
 import colorgram
 from turtle import Turtle, Screen
+import turtle as t
 
 
 def extract_colors_from_image():
@@ -23,18 +26,19 @@ def colors_to_rgb_format(colors):
 #colors = extract_colors_from_image()
 #rgb = colors_to_rgb_format(colors)
 
-image_color_list =[(178, 159, 17), (23, 56, 24), (123, 116, 14), (249, 213, 52), (41, 92, 17), (245, 169, 28), (70, 102, 10), (253, 239, 192), (240, 75, 31), (58, 118, 18
-)]
+#image_color_list =[(178, 159, 17), (23, 56, 24), (123, 116, 14), (249, 213, 52), (41, 92, 17), (245, 169, 28), (70, 102, 10), (253, 239, 192), (240, 75, 31), (58, 118, 18
+#)]
+
 
 # Configure screen and set the correct screen size. The requirements are 10 dots of 20 with 50 in between
-canvas_size = (10*20) + (9*50)
+canvas_size = (10*20) + (9*50) + 10
 print(canvas_size)
 screen = Screen()
 screen.screensize(canvas_size, canvas_size)
 
 
 # Configure Painter and add it to correct position
-painter = Turtle()
+painter = t.Turtle()
 painter.hideturtle()
 painter.penup()
 painter.shape("arrow")
@@ -43,12 +47,49 @@ painter.setx(-315)
 painter.sety(-315)
 painter.showturtle()
 painter.pendown()
+t.colormode(255)
+
+
+# Create Turtle move function
+def move_painter():
+    painter.penup()
+    painter.forward(70)
+    painter.pendown()
+
+
+# Draw a circle
+def draw_filled_circle():
+    image_color_list = [(178, 159, 17), (23, 56, 24), (123, 116, 14), (249, 213, 52), (41, 92, 17), (245, 169, 28),
+                        (70, 102, 10), (253, 239, 192), (240, 75, 31), (58, 118, 18)]
+    color = (image_color_list[random.randint(0, 9)])
+    painter.dot(20, color)
+
+
+# Go to new row
+def new_row():
+    painter.hideturtle()
+    painter.penup()
+    painter.sety(painter.ycor()+70)
+    painter.setx(-315)
+    painter.pendown()
+    painter.showturtle()
+
+
+dot_counter = 0
+for _ in range(100):
+
+    if dot_counter % 10 == 0:
+        new_row()
+
+    draw_filled_circle()
+    move_painter()
+    dot_counter += 1
+
 
 # Canvas only closed when user clicks on it
 screen.exitonclick()
 
 
-# Create Turtle move function
 
 
 
